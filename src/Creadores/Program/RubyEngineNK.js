c10f56d7baad707ecda46b57b7cea1975d7ee34a
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-02-27 13:30:16 -0600

    Create RubyEngineNK.js

---
+++ src/Creadores/Program/RubyEngineNK.js
@@ -0,0 +1,7 @@
+script.registerScript({
+    name: "RubyEngineNK",
+    version: "1.0",
+    description: "Run Ruby on Nukkit!",
+    website: "https://github.com/Trollhunters501/RubyEngineNK/",
+    authors: ["Creadores Program"]
+});

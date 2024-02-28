c745048131ea2d3dbb2bc279c1c467bc1a770e73
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-02-27 19:49:20 -0600

    Update RubyEngineNK.js

--- src/Creadores/Program/RubyEngineNK.js
+++ src/Creadores/Program/RubyEngineNK.js
@@ -1,3 +1,4 @@
+if(manager.getPlugin("RubyLib")){
 script.registerScript({
     name: "RubyEngineNK",
     version: "1.0",
@@ -7,10 +8,17 @@ script.registerScript({
 });
 const RubyEngineNK = Class(Object, {
     build: function(){
-        let libs = new NnClassLoader({ urls: [''] });
+        let libs = new NnClassLoader({ jars: [server.getPluginPath() + 'RubyLib_v9.4.6.0.jar'] });
         let RubyFactor = libs.type();
         let RubyManager = new RubyFactor();
         let RubyEngine = RubyManager.getScriptEngine();
         RubyEngine.put("$getServer", server);
+        RubyEngine.put("$getLogger", console);
+        RubyEngine.put("$manager", manager);
+        RubyEngine.put("$plugin", plugin);
     }
 });
+}else{
+    console.critical("RubyLib Plugin not found! Download at https://github.com/Trollhunters501/RubyLib/releases/tag/9.4.6.0");
+    throw "Error Plugin RubyLib Not Found";
+}

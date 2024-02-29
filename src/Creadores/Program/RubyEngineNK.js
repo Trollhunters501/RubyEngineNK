db1384b5ef28cb72b9a417d3ca1bc4e2b2538b27
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-02-28 23:01:32 -0600

    Update RubyEngineNK.js

--- src/Creadores/Program/RubyEngineNK.js
+++ src/Creadores/Program/RubyEngineNK.js
@@ -1,21 +1,74 @@
-if(manager.getPlugin("RubyLib")){
+if(manager.getPlugin("RubyLib") != null){
 script.registerScript({
     name: "RubyEngineNK",
     version: "1.0",
     description: "Run Ruby on Nukkit!",
     website: "https://github.com/Trollhunters501/RubyEngineNK/",
-    authors: ["Creadores Program"]
+    author: "Creadores Program"
 });
-const RubyEngineNK = Class(Object, {
+var RubyEngineNK = Class(Object, {
     build: function(){
         let libs = new NnClassLoader({ jars: [server.getPluginPath() + 'RubyLib_v9.4.6.0.jar'] });
-        let RubyFactor = libs.type();
+        let RubyFactor = libs.type("org.jruby.embed.jsr223.JRubyEngineFactory");
         let RubyManager = new RubyFactor();
         let RubyEngine = RubyManager.getScriptEngine();
         RubyEngine.put("$getServer", server);
         RubyEngine.put("$getLogger", console);
         RubyEngine.put("$manager", manager);
         RubyEngine.put("$plugin", plugin);
+        let subclass = {
+            put: function(arg1, arg2){
+                RubyEngine.put(arg1, arg2);
+            },
+            eval: function(arg1, arg2){
+                RubyEngine.eval(arg1, arg2);
+            },
+            getEngineName: function(){
+                return "RubyEngineNK & " + RubyManager.getEngineName();
+            },
+            setNnClassLoader: function(codeNC, classes){
+              let ClasLoad = new NnClassLoader(codeNC);
+              let KeysClass = Object.keys(classes);
+              for(var keys in KeysClass){
+                RubyEngine.put(KeysClass[keys], ClasLoad.type(classes[KeysClass[keys]]));
+              }
+            },
+            getLanguageVersion: function(){
+                return RubyManager.getLanguageVersion();
+            },
+            getManager: function(){
+                return RubyManager;
+            },
+            getEngine: function(){
+                return RubyEngine;
+            },
+            getClass: function(){
+                return this;
+            },
+            getEngineClass: function(){
+                return RubyFactor;
+            },
+            evalFile: function(fileA){
+              let FilesImport = new JavaImporter(java.io);
+              with(FilesImport){
+                let reader = new InputStreamReader(new FileInputStream(fileA));
+                RubyEngine.eval(reader);
+              }
+            }
+        };
+        return subclass;
+    },
+    getClass: function(){
+      return this;
+    },
+    constructor: function(){
+      return this.build();
+    },
+    getName: function(){
+      return "RubyEngineNK";
+    },
+    toString: function(){
+      return "RubyEngineNK[]";
     }
 });
 }else{
